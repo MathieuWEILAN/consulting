f007f12b6af615a6218423e0d6268be4338acebd
Mathieu WEI LAN <mathieu_weilan@hotmail.fr>
2025-09-24 12:22:32 +0200

    correct linter

--- src/app/contact/contact.tsx
+++ src/app/contact/contact.tsx
@@ -47,9 +47,9 @@ const Contact = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.9214616995155!2d2.7957320500000002!3d48.840636749999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e602b60e703b3d%3A0x178020d5d986b2ba!2s7%20Av.%20Christian%20Doppler%2C%2077700%20Serris!5e0!3m2!1sfr!2sfr!4v1758704129599!5m2!1sfr!2sfr"
             height="450"
             style={{ border: "0" }}
-            allowfullscreen
+            allowFullScreen
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </section>
